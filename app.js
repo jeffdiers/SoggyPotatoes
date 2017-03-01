6cff04c3328d52d17af48105147d947f6b73c0cf
jeffdiers <jeff.diers@gmail.com>
2017-03-01 11:20:33 -0700

    cors

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/api/posts', posts);
 app.use('/users', users);
